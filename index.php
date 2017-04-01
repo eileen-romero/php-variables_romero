6f7b0d925e347ccee2e86f20c6aa605394384266
eileen-romero <paperfox13@Eileens-MacBook-Air.local>
2017-04-01 09:31:47 -0700

    Initial upload
    Has updated code written by me.

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+  $title = 'Variables';
+  $name = 'Eileen';
+  $sum = 4+4;
+  $sum2 = 3+3;
+  $sum3 = $sum + $sum2;
+  $sum4 = $sum * $sum2;
+
+  // Let's get the age based on birth year
+  $currentYear = date("Y");
+  $birthYear = 1995;
+  $roughAge = $currentYear - $birthYear;
+  $theFuture = 2056;
+
+  // Let's get the real age based on dates
+   // $dob = '03/27/1995';
+   // $dob = explode("/", $dob); 
+   // $curMonth = date("m");
+   // $curDay = date("j");
+   // $curYear = date("Y");
+   // $age = $curYear - $dob[2]; 
+   // if($curMonth<$dob[0] || ($curMonth==$dob[0] && $curDay<$dob[1])) 
+   // $age--; 
+
+  // @param string, date of birth in mm/dd/yyyy format
+  function getAge($dob) {
+
+    // Convert the string into an array, using the / as a separator
+    $dob=explode("/", $dob);
+
+    // Get the current month (based on server clock)
+    $curMonth = date("m");
+
+    // Get the current day (based on server clock)
+    $curDay = date("j");
+
+    // Get the current year (based on server clock)
+    $curYear = date("Y");
+
+    // Current age is equal to current year minus birth year
+   $age = $curYear - $dob[2]; 
+
+    // If the current month is less than the birth month,
+    // OR the current month is the same as the month month,
+    // but the birth day is less than the current day, then
+    // subtract one from the age
+    if($curMonth<$dob[0] || ($curMonth==$dob[0] && $curDay<$dob[1]))  {
+      $age--;
+    }
+
+    // Return the current age
+    return $age; 
+  }
+
+  function ageInTheFuture($dob,$future) {
+    $dob = explode("/", $dob);
+
+    $future = explode("/", $future);
+
+    $futureAge = $future[2] - $dob[2];
+
+    if($dob[0]<$future[0] || ($dob[0] == $future[0] && $dob[1]<$future[1]) ){
+        $futureAge--;
+    }
+
+    return $futureAge;
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+  <body>
+    <main class="container py-4">
+      <h1 class="pb-4">Hello, <?php echo $name; ?></h1>
+      <?php echo '<p>You were born in '.$birthYear.', so you&rsquo;re about '.$roughAge.' years old.</p>'; ?>
+      <?php echo '<p>Your actual age is '.getAge('03/27/1995').'.</p>'; ?>
+      
+      <?php echo '<p> This means on July 23, 2054 you\'ll be ' .ageInTheFuture('03/27/1995','07/23/2054').'.</p>'; ?>
+      
+      <p><strong>The first <code>$sum</code> variable is:</strong> <?php echo $sum; ?></p>
+      <p><strong>The second <code>$sum2</code> variable is:</strong> <?php echo $sum2; ?></p>
+      <p><strong>The third <code>$sum3</code> variable is:</strong> <?php echo $sum3; ?></p>
+      <p><strong> The fourth <code>$sum4</code> variable is:</strong>
+      <?php echo $sum4; ?></p>
+      
+    </main>
+  </body>
+</html>
